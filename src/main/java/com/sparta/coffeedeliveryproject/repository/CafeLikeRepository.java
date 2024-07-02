7d55376022161bb127b866768ce061089ac38dd9
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-07-02 17:56:21 +0900

    ✨ update - 유저가 좋아요한 카페를 찾기 위해 생성

--- src/main/java/com/sparta/coffeedeliveryproject/repository/CafeLikeRepository.java
+++ src/main/java/com/sparta/coffeedeliveryproject/repository/CafeLikeRepository.java
@@ -1,10 +1,15 @@
 package com.sparta.coffeedeliveryproject.repository;
 
 import com.sparta.coffeedeliveryproject.entity.CafeLike;
+import com.sparta.coffeedeliveryproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CafeLikeRepository extends JpaRepository<CafeLike, Long> {
     Optional<CafeLike> findByCafeCafeIdAndUserUserId(Long cafeId, Long userId);
+
+    // 유저로 찾기위해 생성
+    List<CafeLike> findByUser(User user);
 }
